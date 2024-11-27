6a4d6f3e99a2a643f1d9fdfffeeb1fc902c24ff1
Matthew Powell <matt.powell2784@gmail.com>
2024-11-27 17:57:54 +0000

    stlye: add revolut logo

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,5 +1,11 @@
+import revolutLogo from '../assets/revolut_logo.svg'
+
 const App = () => {
-  return <div className="relative overflow-hidden bg-white">App</div>
+  return (
+    <div className="relative flex w-full items-center justify-center overflow-hidden bg-white">
+      <img src={revolutLogo} alt="Revolut Logo" className="m-2" />
+    </div>
+  )
 }
 
 export default App
