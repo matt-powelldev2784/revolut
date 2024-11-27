d1937d764a3292b909f45965a30f12515670b4db
Matthew Powell <matt.powell2784@gmail.com>
2024-11-27 18:51:31 +0000

    feat: add app context

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,6 +1,12 @@
+import { useAppContext } from 'context/AppContext'
 import revolutLogo from '../assets/revolut_logo.svg'
 
 const App = () => {
+  const { usdBalance, gbpBalance, eurBalance } = useAppContext()
+  console.log('usdBalance', usdBalance)
+  console.log('gbpBalance', gbpBalance)
+  console.log('eurBalance', eurBalance)
+
   return (
     <div className="relative flex w-full items-center justify-center overflow-hidden bg-white">
       <img src={revolutLogo} alt="Revolut Logo" className="m-2" />
---
+++ src/context/AppContext.tsx
@@ -0,0 +1,48 @@
+import React, {
+  createContext,
+  ReactNode,
+  useContext,
+  useState,
+  Dispatch,
+  SetStateAction
+} from 'react'
+
+interface AppContext {
+  eurBalance: number
+  setEurBalance: Dispatch<SetStateAction<number>>
+  gbpBalance: number
+  setGbpBalance: Dispatch<SetStateAction<number>>
+  usdBalance: number
+  setUsdBalance: Dispatch<SetStateAction<number>>
+}
+
+export const AppContext = createContext<AppContext | undefined>(undefined)
+
+export const useAppContext = () => {
+  const context = useContext(AppContext)
+  if (!context) {
+    throw new Error('App context must be used within a context provider')
+  }
+  return context
+}
+
+export const AppContextProvider = ({ children }: { children: ReactNode }) => {
+  const [eurBalance, setEurBalance] = useState(50)
+  const [gbpBalance, setGbpBalance] = useState(50)
+  const [usdBalance, setUsdBalance] = useState(50)
+
+  return (
+    <AppContext.Provider
+      value={{
+        eurBalance,
+        setEurBalance,
+        gbpBalance,
+        setGbpBalance,
+        usdBalance,
+        setUsdBalance
+      }}
+    >
+      {children}
+    </AppContext.Provider>
+  )
+}
