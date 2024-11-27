875ea1c6855d4befdb1c7d740e047d84fdb67942
Matthew Powell <matt.powell2784@gmail.com>
2024-11-27 18:59:08 +0000

    feat: display account balances

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -3,14 +3,26 @@ import revolutLogo from '../assets/revolut_logo.svg'
 
 const App = () => {
   const { usdBalance, gbpBalance, eurBalance } = useAppContext()
-  console.log('usdBalance', usdBalance)
-  console.log('gbpBalance', gbpBalance)
-  console.log('eurBalance', eurBalance)
 
   return (
-    <div className="relative flex w-full items-center justify-center overflow-hidden bg-white">
+    <main className="relative flex w-full flex-col items-center justify-center overflow-hidden bg-white">
       <img src={revolutLogo} alt="Revolut Logo" className="m-2" />
-    </div>
+
+      <section className="flex flex-col items-center">
+        <h1 className="text-lg font-bold">Account Balances</h1>
+        <div className="flex flex-row text-lg">
+          <p className="mx-4">
+            EUR: <span className="text-blue-600">€{eurBalance}</span>
+          </p>
+          <p className="mx-4">
+            GBP: <span className="text-blue-600">€{gbpBalance}</span>
+          </p>
+          <p className="mx-4">
+            USD: <span className="text-blue-600">${usdBalance}</span>
+          </p>
+        </div>
+      </section>
+    </main>
   )
 }
 
